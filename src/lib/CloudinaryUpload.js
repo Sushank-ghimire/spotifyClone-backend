12dc48dc7e02d18671edce00298470b1b7dbc316
Sushank Ghimire <ghimiresushank08@gmail.com>
2025-02-07 08:05:01 -0800

    Deleted all files from local System After completing upload to the cloudinary or if getting an error.

--- src/lib/CloudinaryUpload.js
+++ src/lib/CloudinaryUpload.js
@@ -1,12 +1,15 @@
 import cloudinary from "./cloudinary.js";
+import fs from "fs";
 
 const uploadToCloudinary = async (file) => {
   try {
     const result = await cloudinary.uploader.upload(file.tempFilePath, {
       resource_type: "auto",
     });
+    fs.unlinkSync(file.tempFilePath);
     return result.secure_url;
   } catch (error) {
+    fs.unlinkSync(file.tempFilePath);
     throw Error("Failed to upload to cloudinary : ", error.message);
   }
 };
